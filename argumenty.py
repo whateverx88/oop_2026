fadd19784ee84b30c6bc0aa72797b3d8c3f0372b
whateverx88 <whateverx88@gmail.com>
2026-01-03 14:55:46 +0100

    aktualizacja

--- argumenty.py
+++ argumenty.py
@@ -3,7 +3,7 @@
 def dodaj(a, b):
     return a+b
 dodaj(6, 4)
-#
+
 # *args - wiele argumentów
 def dodaj2(*args):
     # Zmienna lokalna wynik
@@ -16,29 +16,36 @@ print(dodaj2(2, 3))
 print(dodaj2(2,4,5))
 print(dodaj2(2,4,5,5,6,7))
 
+# #Argument 'verbose' o domyślnej wartości
 def dodaj3(*args, verbose=False):
-    # Zmienna lokalna wynik
     if verbose == True:
-        print("Wykonam działanie dodawania")
+        print(f"Wykonam działanie dodawania. Dodam {args}")
     wynik = 0
     for arg in args:
         wynik += arg
     return wynik
-
+#
 print(dodaj3(2, 3))
 print(dodaj3(2,4,5))
 print(dodaj3(2,4,5,5,6,7))
 print(dodaj3(2,4,5,5,6,7, verbose=True))
+
+# Argumenty słownikowe
+def funkcja(**kwargs):
+    print(type(kwargs))
+    print(kwargs)
+
+funkcja(verbose=True, parametr="wartosc")
+funkcja(verbose=True)
 #
-# # Zmienna globalna wynik
-# wynik = dodaj3(1, 2, 3, 4, verbose=True)
-# print(wynik)
-#
-# print("Adam")
-# print("Adam", "Ewa")
-# print("Adam", "Ewa", sep="przerwa")
-# print("Adam", "Ewa", sep="przerwa", end="koniec")
-# # domyślnie p
-# print("Adam", sep=" ", end="\n")
-#
-# print(dodaj3(1,2))
+# Zmienna globalna wynik
+wynik = dodaj3(1, 2, 3, 4, verbose=True)
+print(wynik)
+
+
+# Przykład na funkcji wbudowanej print
+print("Adam")
+print("Adam", "Ewa")
+print("Adam", "Ewa", sep="przerwa")
+print("Adam", "Ewa", sep="przerwa", end="koniec")
+print("Adam", sep=" ", end="\n")
