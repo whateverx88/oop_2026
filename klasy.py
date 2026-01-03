fb6a573b3da144ebfeded4d9b5bd98a837a2ae4a
whateverx88 <whateverx88@gmail.com>
2026-01-03 11:13:44 +0100

    update

--- klasy.py
+++ klasy.py
@@ -2,15 +2,19 @@
 class Czlowiek:
     #Istota
     gatunek = "Homo Sapiens"
-    def __init__(self):
+    def __init__(self, imie):
     #Konstruktor
     #Akt istnienia
-        print ("Niech powstanie Czlowiek")
-
+        print (f"Niech powstanie Czlowiek o imieniu {imie}")
+        self.imie = imie
 
 #powstanie oboektu, gotowanie z przepisu
-adam = Czlowiek()
+adam = Czlowiek("Adam")
+ewa = Czlowiek("Ewa")
 #print(type(adam))
 #print(dir(adam))
 #print(dir(Czlowiek))
 print(adam.gatunek)
+print(ewa.gatunek)
+print(ewa.imie)
+print(adam.imie)
