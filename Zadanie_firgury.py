375c9295daf8a720a8974967cb21490d07c34ac3
whateverx88 <whateverx88@gmail.com>
2026-01-04 14:43:33 +0100

    zadanie update

--- Zadanie_firgury.py
+++ Zadanie_firgury.py
@@ -1,6 +1,8 @@
 class FiguraGeometryczna:
     def policz_pole(self):
         pass
+    def policz_obwod(self):
+        pass
 
 class Prostokat(FiguraGeometryczna):
     def __init__(self, a, b):
@@ -8,14 +10,21 @@ class Prostokat(FiguraGeometryczna):
         self.b = b
     def policz_pole(self):
         return self.a * self.b
+    def policz_obwod(self):
+        return 2 * self.a + 2 * self.b
 
 class Kwadrat(FiguraGeometryczna):
     def __init__(self, a):
         self.a = a
     def policz_pole(self):
         return self.a ** 2
+    def policz_obwod(self):
+        return self.a * 4
 
-prostokat = Prostokat(2,3)
-kwadrat = Kwadrat(a=4)
+prostokat = Prostokat(3,3)
+kwadrat = Kwadrat(a=5)
+
 print("Pole prostokata wynosi:", prostokat.policz_pole())
+print("Obwod prostokata wynosi:", prostokat.policz_obwod())
 print("Pole kwadratu wynosi:", kwadrat.policz_pole())
+print("Obwod kwadratu wynosi:", kwadrat.policz_obwod())
